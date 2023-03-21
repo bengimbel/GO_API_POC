9a3eb7ff751d47b8f90e5de966fab81463a4fe1d
Ben Gimbel <bengimbel@bens-mbp-m1.lan>
2023-03-21 14:58:58 -0500

    removing comments

--- pkg/routes/user-routes.go
+++ pkg/routes/user-routes.go
@@ -7,12 +7,6 @@ import (
 )
 
 var RegisterUserRoutes = func(router *mux.Router) {
-	// router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	// router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	// router.HandleFunc("/book/{bookId}", router.Use(middleware(controllers.GetBookById))).Methods("GET")
-	// router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	// router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PATCH")
-	// router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 	router.HandleFunc("/register", controllers.RegisterUser).Methods("POST")
 	router.HandleFunc("/login", controllers.GenerateToken).Methods("POST")
 }
